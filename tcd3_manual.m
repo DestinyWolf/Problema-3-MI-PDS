d73981d8874800e03a6ab3332d3ee0ab8079a14b
Camila Boa Morte <116687830+camilaqPereira@users.noreply.github.com>
2026-07-08 12:25:11 -0300

    Refatorando tcd3_manual

--- tcd3_manual.m
+++ tcd3_manual.m
@@ -1,32 +1,16 @@
-% ======================================================
-% Arquivo: tcd3_manual.m
-% Autor: Camila Boa Morte e Maike de Oliveira
-% Descrição: Esta função implementa o cálculo da
-% TCD-III ortonormalizada com base na definição analítica
-%
-% Parametros de entrada:
-% x: amostras do sinal de entrada
-%
-% Parametros de saida:
-% X: coeficientes da Transformada Cosseno Discreta III
-% ======================================================
-
+%% Função para calcular a Transformada Discreta do Cosseno (DCT) de um sinal de entrada
 
 function X = tcd3_manual(x)
   x = x(:).';       % Formatação do sinal de entrada para um vetor linha
   N = length(x);    % Comprimento do sinal de entrada
-
-  % =====================================
-  % Cálculo da TCD-III por definição
-  % =====================================
 
   X = zeros(1,N);   % Inicialização do vetor de saída
 
   n = 1:(N-1);      % Vetor de índices n
 
   for k=0 : N-1
-    somatorio = sum(x(n+1) .* cos((pi/N) * (k + 0.5) * n));
-    X(k+1) = (sqrt(1/N) * x(1)) + (sqrt(2/N) * somatorio);
+    somatorio_interno = sum(x(n+1) .* cos((pi/N) * (k + 0.5) * n));
+    X(k+1) = (sqrt(1/N) * x(1)) + (sqrt(2/N) * somatorio_interno);  % Normalização do somatorio
 
   end
 
